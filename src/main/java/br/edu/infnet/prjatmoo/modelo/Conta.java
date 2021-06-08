e33d7fb69c91ec721c56863e15d06f1febaf1964
oliv4 <oliva.git@hotmail.com>
2021-06-07 22:13:53 -0300

    feat: add account model singleton pattern

--- src/main/java/br/edu/infnet/prjatmoo/modelo/Conta.java
+++ src/main/java/br/edu/infnet/prjatmoo/modelo/Conta.java
@@ -3,24 +3,32 @@ package br.edu.infnet.prjatmoo.modelo;
 import java.util.ArrayList;
 import java.util.List;
 
-import br.edu.infnet.prjatmoo.utils.LoginOrSenhaInvalidaExcetion;
-
 public class Conta {
-	
+
 	/**
 	 * Classe que cria a conta 
 	 * @param numero conta do banco
 	 * @param saldo da conta
 	 */
-	 
+
+	private static Conta instancia;
+
 	private String numero;
 	
 	private int pin;
 	
 	private double saldo;
 	
-	private List<Transacao> transacoes = new ArrayList<Transacao>();
+	private static List<Transacao> transacoes = new ArrayList<Transacao>();
 	
+	public static Conta getInstancia(String numero, int pin) {
+		if(instancia == null) {
+			instancia = new Conta(numero, pin);
+		}
+
+		return instancia;
+	}
+
 	public Conta(String numero, int pin) {
 		this.numero = numero;
 		this.pin = pin;
@@ -46,21 +54,12 @@ public class Conta {
 		return transacoes;
 	}
 
-	public void setTransacoes(List<Transacao> transacoes) {
-		this.transacoes = transacoes;
-	}
-
 	public void addTransacao(Transacao transacao) {
-		if(getTransacoes() == null) {
-			this.transacoes = new ArrayList<Transacao>();
-		}
-		getTransacoes().add(transacao);
+		transacoes.add(transacao);
 	}
 
 	@Override
 	public String toString() {
 		return "Conta [numero=" + numero + ", pin=" + pin + ", saldo=" + saldo + ", transacoes=" + transacoes + "]";
 	}
-	
-
 }
